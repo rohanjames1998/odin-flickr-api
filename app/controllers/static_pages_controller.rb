5950e62ccd7df0542dd75ab6fee85e2770a97b9e
Rohan James <rohanjames9282@gmail.com>
2023-03-04 13:58:25 +0530

    Add logic to get user photos after giving user id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if params[:flickr]
+      flickr = Flickr.new
+      @photos = flickr.people.getPhotos api_key: ENV["FLICKR_API_KEY"], user_id: params[:flickr][:user_id]
+    end
   end
 end
