032794743280c41f631136bbfb68b4f803c7224b
Rohan James <rohanjames9282@gmail.com>
2023-03-04 16:16:58 +0530

    Add rescue bloc when given user id is invalid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
     if params[:flickr]
-      flickr = Flickr.new
-      @photos = flickr.people.getPhotos api_key: ENV["FLICKR_API_KEY"], user_id: params[:flickr][:user_id]
+      @flickr = Flickr.new
+      begin
+      @photos = @flickr.people.getPhotos api_key: ENV["FLICKR_API_KEY"], user_id: params[:flickr][:user_id]
+      rescue Flickr::FailedResponse
+        flash[:notice] = "User not found"
+        render :home
+      end
     end
   end
 end
